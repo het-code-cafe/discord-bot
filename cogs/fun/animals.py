a6abd126dc2e894ea99ebfcdb0e5c0f832d4f026
Joris van Breugel <jorisvanbreugel@mailbox.org>
2024-02-12 17:02:35 +0100

    Something with tabs and spaces. I'm lost without a compiler

--- cogs/fun/animals.py
+++ cogs/fun/animals.py
@@ -2,6 +2,7 @@ import discord, requests, random
 from discord.ext import commands
 from helpers import imgur
 
+
 class Animals(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -9,7 +10,7 @@ class Animals(commands.Cog):
     @commands.command(aliases=["katje", "kitty"])
     async def kat(self, ctx):
         """
-            Gets a random cat image from thecatapi.com
+        Gets a random cat image from thecatapi.com
         """
         req = requests.get("https://api.thecatapi.com/v1/images/search").json()
         msg = req[0]["url"]
@@ -20,48 +21,48 @@ class Animals(commands.Cog):
         else:
             embed.set_image(url=msg)
         await ctx.send(embed=embed)
-        
+
     @commands.command(aliases=["pandie"])
     async def panda(self, ctx):
         """
-            Gets a random panda image from some-random-api.ml
+        Gets a random panda image from some-random-api.ml
         """
         req = requests.get("https://some-random-api.ml/img/panda").json()
         msg = req["link"]
         embed = discord.Embed(title="🐼 A panda for you!", color=0x000000)
         embed.set_image(url=msg)
         await ctx.send(embed=embed)
-        
+
     @commands.command(aliases=["doggo", "woof"])
     async def dog(self, ctx):
         """
-            Gets a random dog image from thedogapi.com
+        Gets a random dog image from thedogapi.com
         """
         req = requests.get("https://api.thedogapi.com/v1/images/search").json()
         msg = req[0]["url"]
         embed = discord.Embed(title="🐶 Woof! Here's a dog for you!", color=0x8B4513)
         embed.set_image(url=msg)
         await ctx.send(embed=embed)
-        
+
     @commands.command(aliases=["foxie", "foxxie"])
     async def fox(self, ctx):
         """
-            Gets a random fox image from randomfox.ca
+        Gets a random fox image from randomfox.ca
         """
         req = requests.get("https://randomfox.ca/floof/").json()
         msg = req["image"]
         embed = discord.Embed(title="🦊 A fox for you!", color=0xFF5733)
         embed.set_image(url=msg)
         await ctx.send(embed=embed)
-        
+
     @commands.command()
     async def newpanda(self, ctx):
-	    res: tuple | None = helpers.imgur_search("panda")
-	    if res is not None:
-    		img, title = res
-            	embed = discord.Embed(title=f"🐼 {title}", color=0x000000)
-            	embed.set_image(url=img)
-            	await ctx.send(embed=embed)
+        res: tuple | None = helpers.imgur_search("panda")
+        if res is not None:
+            img, title = res
+            embed = discord.Embed(title=f"🐼 {title}", color=0x000000)
+            embed.set_image(url=img)
+            await ctx.send(embed=embed)
 
     # @commands.command(aliases=["birb", "birdie"])
     # async def bird(self, ctx):
@@ -73,48 +74,74 @@ class Animals(commands.Cog):
     #     embed = discord.Embed(title="🐦 A bird for you!", color=0x000000)
     #     embed.set_image(url=msg)
     #     await ctx.send(embed=embed)
-        
+
     @commands.command(aliases=["rabbit"])
     async def bunny(self, ctx):
         """
-            Gets a random bunny image from api.bunnies.io
+        Gets a random bunny image from api.bunnies.io
         """
         media_type = "gif" if random.random() < 0.5 else "mp4"
-        req = requests.get(f"https://api.bunnies.io/v2/loop/random/redirect/?media={media_type}")
+        req = requests.get(
+            f"https://api.bunnies.io/v2/loop/random/redirect/?media={media_type}"
+        )
         msg = req.url
         embed = discord.Embed(title="🦆 A bunny for you!", color=0x000000)
         embed.set_image(url=msg)
         await ctx.send(embed=embed)
-        
+
     @commands.command(aliases=["duckie", "quack"])
     async def duck(self, ctx):
         """
-            Gets a random duck image from random-d.uk
+        Gets a random duck image from random-d.uk
         """
         req = requests.get("https://random-d.uk/api/v2/random").json()
         msg = req["url"]
         embed = discord.Embed(title="🦆 A duck for you!", color=0x000000)
         embed.set_image(url=msg)
         await ctx.send(embed=embed)
-        
-        
-    #? Pokemon is not an animal but it fits the theme. Agreed?
+
+    # ? Pokemon is not an animal but it fits the theme. Agreed?
     @commands.command(aliases=["poke", "pkmn", "pokémon", "pokèmon"])
     async def pokemon(self, ctx, *pokemon):
         """
-            Gets a random pokemon image from pokeapi.co
-        """                
-        req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon[0].lower()}").json()
-        embed = discord.Embed(title=f"🐾 {pokemon[0].capitalize()} for you!", color=0xFF5733)
+        Gets a random pokemon image from pokeapi.co
+        """
+        req = requests.get(
+            f"https://pokeapi.co/api/v2/pokemon/{pokemon[0].lower()}"
+        ).json()
+        embed = discord.Embed(
+            title=f"🐾 {pokemon[0].capitalize()} for you!", color=0xFF5733
+        )
         embed.add_field(name="Name", value=pokemon[0].capitalize(), inline=True)
-        embed.add_field(name="Type", value=req.get('types')[0].get('type').get('name').title(), inline=True)
-        embed.add_field(name="Health", value=req.get('stats')[0].get('base_stat'), inline=True)
-        embed.add_field(name="Attack", value=req.get('stats')[1].get('base_stat'), inline=True)
-        embed.add_field(name="Defense", value=req.get('stats')[2].get('base_stat'), inline=True)
-        embed.add_field(name="Special Attack", value=req.get('stats')[3].get('base_stat'), inline=True)
-        embed.add_field(name="Special Defense", value=req.get('stats')[4].get('base_stat'), inline=True)
-        embed.add_field(name="Speed", value=req.get('stats')[5].get('base_stat'), inline=True)
+        embed.add_field(
+            name="Type",
+            value=req.get("types")[0].get("type").get("name").title(),
+            inline=True,
+        )
+        embed.add_field(
+            name="Health", value=req.get("stats")[0].get("base_stat"), inline=True
+        )
+        embed.add_field(
+            name="Attack", value=req.get("stats")[1].get("base_stat"), inline=True
+        )
+        embed.add_field(
+            name="Defense", value=req.get("stats")[2].get("base_stat"), inline=True
+        )
+        embed.add_field(
+            name="Special Attack",
+            value=req.get("stats")[3].get("base_stat"),
+            inline=True,
+        )
+        embed.add_field(
+            name="Special Defense",
+            value=req.get("stats")[4].get("base_stat"),
+            inline=True,
+        )
+        embed.add_field(
+            name="Speed", value=req.get("stats")[5].get("base_stat"), inline=True
+        )
         await ctx.send(embed=embed)
-    
+
+
 async def setup(bot):
     await bot.add_cog(Animals(bot))
