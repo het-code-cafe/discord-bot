8b9c10481c0846d8cbaabe05494c2d98dd2e0992
Lina Blijleven <lina.blijleven@hu.nl>
2024-02-20 17:32:23 +0100

    PEP

--- cogs/system/error.py
+++ cogs/system/error.py
@@ -1,5 +1,5 @@
-import discord
 from discord.ext import commands
+
 
 class ErrorHandler(commands.Cog):
     def __init__(self, bot):
@@ -12,6 +12,7 @@ class ErrorHandler(commands.Cog):
         # Handle other errors or re-raise them if not handled
         else:
             raise error
+
 
 async def setup(bot):
     await bot.add_cog(ErrorHandler(bot))
